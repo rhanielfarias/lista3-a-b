4029a68e9273c90a8f56e7b597025bcf686db22c
rhanielfarias <rhanielfarias@gmail.com>
2022-06-28 13:59:54 -0300

    Primeiro exercício da lista B. Usando for e While para contar de 1 a 100

---
+++ Contador.java
@@ -0,0 +1,15 @@
+    public class Contador {
+
+    public static void main(String [] args) {
+
+int contador = 0;
+        while (contador < 101) {
+System.out.println(contador);
+contador++;
+}
+
+for (int i = 0; i < 101; i++)
+System.out.println(i);
+
+}
+}
