9c0f08ddca2b9e4327a06df58761e55c4a53cc63
rhanielfarias <rhanielfarias@gmail.com>
2022-06-28 20:42:28 -0300

    oitavo exercício B. Somando impar de 0 a 50

---
+++ Impar.java
@@ -0,0 +1,13 @@
+
+    public class Impar {
+    public static void main(String [] args) {
+double soma=0;
+System.out.println("Somando número impar de 0 a 50");
+for(int i = 0; i < 51; i++) {
+if(i%2 == 1  ) {
+soma=soma+i;
+}
+System.out.println(soma);
+}
+}
+}
